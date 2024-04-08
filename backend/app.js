a582edd49e601142f7632a7d15298c7c70fcb430
HUMBEF0OL <123amitrana0123@gmail.com>
2024-04-08 11:54:31 +0530

    feat: established database connection

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
-require('dotenv').config()
-
+require('dotenv').config();
+const mongoose = require('mongoose');
+const quoteRoute = require('./routes/quoteRoutes')
 const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log("listening on: ", process.env.PORT)
-});
+try {
+
+    mongoose.connect(process.env.DB_URL).then((err) => {
+        app.listen(process.env.PORT, () => {
+            console.log("listening on: ", process.env.PORT)
+        });
+    });
+} catch (err) {
+    console.log("connection failed with:  ", err);
+
+}
+
+
+
+app.use('/quote', quoteRoute);
