f50a999be14dd34edbf5352d4b95b8e4b0455f41
HUMBEF0OL <123amitrana0123@gmail.com>
2024-04-09 16:18:17 +0530

    designed the header and footer

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        main: '#00224D',
+        primary: '#5D0E41',
+        secondary: '#A0153E',
+        highlight: '#FF204E'
+      }
     },
   },
   plugins: [],
