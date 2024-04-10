2dd19eeb4c5300f9ff9d4f34c83e1542c48eaad5
HUMBEF0OL <123amitrana0123@gmail.com>
2024-04-10 14:48:19 +0530

    custom snackbar

---
+++ frontend/src/components/Snackbar.tsx
@@ -0,0 +1,41 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import classNames from 'classnames'
+
+const Snackbar = ({ message, mode }: { message: string, mode: 'SUCCESS' | 'ERROR' }) => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        if (isVisible) {
+            let timeout = setTimeout(() => {
+                setIsVisible(false);
+            }, 3000)
+            return () => {
+                clearTimeout(timeout)
+            }
+        }
+    }, [isVisible])
+
+    return (
+        <>
+            {
+                isVisible && (
+                    <div className={`flex h-[40px] w-[250px] ${mode === 'ERROR' ? 'bg-red-100' : 'bg-green-100'} justify-between px-2 items-center right-0 top-0`}>
+                        <p className={` ${mode === 'ERROR' ? 'text-red-700' : 'text-green-700'} text-xl font-semibold`}>{message}!</p>
+                        <button onClick={() => { setIsVisible(false) }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-7 h-7 ${mode === 'SUCCESS' ? 'text-green-700' : 'text-red-700'}`}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                            </svg>
+
+                        </button>
+
+
+                    </div>
+
+                )
+            }
+        </>
+    )
+}
+
+export default Snackbar
